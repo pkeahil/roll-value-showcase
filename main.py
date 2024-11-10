546f2532d85f48ec36f0f8ee0a6f6175bd7c23de
Paul Lupeituu <keahithrive@gmail.com>
2024-11-09 19:33:52 -0600

    fix: adjust test script to output both showcase and player info

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import json
+
 from commands import showcase
+from profiles import profiles
 
 my_uid = "625733723"
 
 result = showcase.get_character_showcase(my_uid)
+result2 = profiles.get_player_info(my_uid)
 
 print(result)
+
+with open("./result.json", "w") as file:
+    json.dump(result, file)
+
+with open("./result2.json", "w") as file:
+    json.dump(result2, file)
