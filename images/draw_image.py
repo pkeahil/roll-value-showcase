7481562e67030f4403beefa057ab2ec875f68129
Paul Lupeituu <keahithrive@gmail.com>
2024-11-23 21:44:57 -0600

    feat: adds user's UID, full character stats, constellations, talents, and weapon

--- images/draw_image.py
+++ images/draw_image.py
@@ -2,12 +2,14 @@
 
 # 50, 250, 450, 650, 850 y
 # 1200 x
+import json
 from io import BytesIO
 
 import requests
 from PIL import Image, ImageDraw, ImageFont
 
 from artifacts import artifacts
+from characters import characters
 
 enka_api = "https://enka.network/"
 
@@ -87,14 +89,224 @@ def draw_artifact_substats(
 
 def draw_splash_art(im: Image, character: str) -> Image:
     splash_art = Image.open(f"images/splash_arts/{character}.png")
+    middle_x = splash_art.width // 2
+    middle_y = splash_art.height // 2
     splash_art = splash_art.crop(
-        (150, 0, splash_art.width - 150, splash_art.height)
+        (middle_x - 200, middle_y - 200, middle_x + 200, middle_y + 200)
     )
-    new_width = int(splash_art.width * 1.5)
+
+    new_width = 1000
     new_height = new_width * splash_art.height // splash_art.width
 
     splash_art = splash_art.resize((new_width, new_height))
-    im.paste(splash_art, (0, 0), splash_art)
+    splash_art.thumbnail((750, 750))
+    im.paste(splash_art, (50, 50), splash_art)
+
+
+def draw_character_stats(
+        im: Image,
+        draw: ImageDraw,
+        font: ImageFont,
+        total_stats: dict,
+        x: int,  # start at 5
+        y: int):  # start at 825
+    draw.rounded_rectangle(
+        (5, 825, 1150, 1025),
+        fill="black",
+        outline="white",
+        width=3,
+        radius=10
+    )
+    im.paste(
+        Image.open("images/stat_icons/FIGHT_PROP_HP.png"),
+        (x + 10, y + 25),
+        Image.open("images/stat_icons/FIGHT_PROP_HP.png")
+    )
+    # paste image of atk on image
+    im.paste(
+        Image.open("images/stat_icons/FIGHT_PROP_ATTACK.png"),
+        (x + 10, y + 75),
+        Image.open("images/stat_icons/FIGHT_PROP_ATTACK.png")
+    )
+    im.paste(
+        Image.open("images/stat_icons/FIGHT_PROP_DEFENSE.png"),
+        (x + 10, y + 125),
+        Image.open("images/stat_icons/FIGHT_PROP_DEFENSE.png")
+    )
+    im.paste(
+        Image.open("images/stat_icons/FIGHT_PROP_CRITICAL.png"),
+        (x + 230, y + 25),
+        Image.open("images/stat_icons/FIGHT_PROP_CRITICAL.png")
+    )
+    im.paste(
+        Image.open("images/stat_icons/FIGHT_PROP_CRITICAL_HURT.png"),
+        (x + 230, y + 75),
+        Image.open("images/stat_icons/FIGHT_PROP_CRITICAL_HURT.png")
+    )
+    im.paste(
+        Image.open("images/stat_icons/FIGHT_PROP_CHARGE_EFFICIENCY.png"),
+        (x + 560, y + 25),  # x + 560, y + 25
+        Image.open("images/stat_icons/FIGHT_PROP_CHARGE_EFFICIENCY.png")
+    )
+    im.paste(
+        Image.open("images/stat_icons/FIGHT_PROP_ELEMENT_MASTERY.png"),
+        (x + 560, y + 75),
+        Image.open("images/stat_icons/FIGHT_PROP_ELEMENT_MASTERY.png")
+    )
+
+    # paste image of def on image
+    draw.text(
+        (65, 850),  # x + 60, y + 25
+        f"HP: {int(total_stats['2000'])}",
+        fill="white",
+        font=font
+    )
+    draw.text(
+        (65, 900),  # x + 60, y + 75
+        f"ATK: {int(total_stats['2001'])}",
+        fill="white",
+        font=font
+    )
+    draw.text(
+        (65, 950),  # x + 60, y + 125
+        f"DEF: {int(total_stats['2002'])}",
+        fill="white",
+        font=font
+    )
+    draw.text(
+        (275, 850),  # x + 270, y + 25
+        f"CRIT Rate: {round(total_stats['20'] * 100, 1)}%",
+        fill="white",
+        font=font
+    )
+    draw.text(
+        (275, 900),  # x + 270, y + 75
+        f"CRIT DMG: {round(total_stats['22'] * 100, 1)}%",
+        fill="white",
+        font=font
+    )
+    draw.text(
+        (615, 850),  # x + 610, y + 25
+        f"Energy Recharge: {round(total_stats['23'] * 100, 1)}%",
+        fill="white",
+        font=font
+    )
+    draw.text(
+        (615, 900),  # x + 610, y + 75
+        f"Elemental Mastery: {int(round(total_stats['28'], 0))}",
+        fill="white",
+        font=font
+    )
+
+
+def draw_character_talents(
+        im: Image,
+        draw: ImageDraw,
+        font: ImageFont,
+        avatarInfo: dict,
+        char_info: dict,
+        x: int,
+        y: int):
+    talent_levels = avatarInfo["skillLevelMap"]
+    talent_ids = [str(talent_id) for talent_id in char_info["SkillOrder"]]
+
+    for talent_id in talent_ids:
+        talent = char_info["Skills"][talent_id]
+        talent_response = requests.get(f"{enka_api}/ui/{talent}.png")
+        talent_icon = (
+            Image.open(
+                BytesIO(talent_response.content)
+            ).resize((80, 80))
+        )
+        talent_level = talent_levels[talent_id]
+
+        if "proudSkillExtraLevelMap" in avatarInfo:
+            extra_levels = avatarInfo["proudSkillExtraLevelMap"]
+            id = str(char_info["ProudMap"][talent_id])
+            if id in extra_levels:
+                talent_level += extra_levels[id]
+
+        draw.circle(
+            (x, y),
+            40,
+            fill="black",
+            outline="white"
+        )
+        im.paste(talent_icon, (x - 40, y - 40), talent_icon)
+        draw.circle(
+            (x + 25, y + 25),
+            20,
+            fill="black"
+        )
+        draw.text(
+            (x + 12, y + 13),
+            f"{talent_level}",
+            fill="white",
+            font=font,
+            align="center"
+        )
+
+        x += 100
+
+
+def draw_character_constellations(
+        im: Image,
+        draw: ImageDraw,
+        avatarInfo: dict,
+        char_info: dict,
+        x: int,
+        y: int):
+    bg_colors = {
+        "Water": "#00BFFF",
+        "Fire": "#EC4923",
+        "Wind": "#359697",
+        "Rock": "#debd6c",
+        "Electric": "#945dc4",
+        "Grass": "#608a00",
+        "Ice": "#4682B4"
+    }
+    constellation = 0
+    if "talentIdList" in avatarInfo:
+        constellation = len(avatarInfo["talentIdList"])
+        print(char_info)
+        print(char_info["Element"])
+        bg_color = bg_colors[char_info["Element"]]
+        for i in range(constellation):
+            const = char_info["Consts"][i]
+            const_response = requests.get(f"{enka_api}/ui/{const}.png")
+            const_icon = (
+                Image.open(
+                    BytesIO(const_response.content)
+                ).resize((80, 80))
+            )
+            draw.circle(
+                (x + 40, y + 40),
+                40,
+                fill=bg_color,
+                outline="white",
+                width=2
+            )
+            im.paste(const_icon, (x, y), const_icon)
+            x += 100
+            if (i + 1) % 3 == 0:
+                # move to next line
+                x = 810
+                y += 100
+
+    for i in range(constellation, 6):
+        locked = Image.open("images/ui/LOCKED.png").resize((40, 40))
+        draw.circle(
+            (x + 40, y + 40),
+            40,
+            fill="black",
+            outline="white",
+        )
+        im.paste(locked, (x + 20, y + 20), locked)
+        x += 100
+        if (i + 1) % 3 == 0:
+            # move to next line
+            x = 810
+            y += 100
 
 
 def draw_roll_value(
@@ -130,8 +342,8 @@ include_percentage_substats = [
 
 def draw_character_showcase(
         character: str,
-        character_id: str,
-        artifacts_list: list) -> Image:
+        avatarInfo: dict,
+        player_uid: str) -> Image:
     # Create base image, initialize font
     im = Image.new("RGB", (1920, 1080), "black")
     draw = ImageDraw.Draw(im)
@@ -140,9 +352,69 @@ def draw_character_showcase(
     # Draw character splash art
     draw_splash_art(im, character)
 
+    # Draw player's uid
+    draw.text(
+        (10, 10),
+        f"UID: {player_uid}",
+        fill="white",
+        font=font
+    )
+
+    # Draw character total stats
+    print(json.dumps(avatarInfo, indent=1))
+    draw_character_stats(im, draw, font, avatarInfo["fightPropMap"], 5, 825)
+
+    # Draw character talents
+    char_info = characters.character_info[str(avatarInfo["avatarId"])]
+    draw_character_talents(im, draw, font, avatarInfo, char_info, 850, 95)
+
+    # Draw character constellations
+    draw_character_constellations(im, draw, avatarInfo, char_info, 810, 175)
+
+    # Draw weapon
+    draw.rounded_rectangle(
+        (805, 400, 1100, 500),
+        radius=10,
+        fill="black",
+        outline="white"
+    )
+    flat = avatarInfo["equipList"][-1]["flat"]
+    weapon_icon = flat["icon"]
+    weapon_response = requests.get(f"{enka_api}/ui/{weapon_icon}.png")
+    weapon = Image.open(BytesIO(weapon_response.content)).resize((100, 100))
+    im.paste(weapon, (800, 400), weapon)
+    weapon_base_atk = flat["weaponStats"][0]
+    atk_icon = Image.open(
+        "images/stat_icons/FIGHT_PROP_ATTACK.png"
+    ).resize((30, 30))
+    im.paste(atk_icon, (910, 410), atk_icon)
+    draw.text(
+        (950, 410),
+        f"{weapon_base_atk['statValue']}",
+        fill="white",
+        font=font
+    )
+
+    secondary_stat = flat["weaponStats"][1]
+    secondary_stat_icon = Image.open(
+        f"images/stat_icons/{secondary_stat['appendPropId']}.png"
+    ).resize((30, 30))
+    secondary_stat_id = secondary_stat["appendPropId"]
+    im.paste(secondary_stat_icon, (910, 460), secondary_stat_icon)
+    draw.text(
+        (950, 460),
+        f"""{secondary_stat['statValue']}{
+            '%' if secondary_stat_id in include_percentage_substats
+            else ''
+        }""",
+        fill="white",
+        font=font
+    )
+
     # Draw artifacts
     x_box = 1200
     y_box = 50
+    artifacts_list = characters.get_artifact_list(avatarInfo)
     for artifact in artifacts_list:
         flat = artifact["flat"]
         if flat["itemType"] == "ITEM_RELIQUARY":
