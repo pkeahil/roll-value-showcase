9a25264bdf6295695a3d5371bb910d923dff025d
Paul Lupeituu <keahithrive@gmail.com>
2025-04-13 11:47:38 -0500

    feat: add functionality to save builds to database

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
+import json
 import os
 
 import discord
+import pandas as pd
 from dotenv import load_dotenv
 
+import builds
+import db
 from commands import showcase
 from views.showcase import FullShowcaseView
 
@@ -28,6 +32,17 @@ async def showcase_rv(ctx: discord.ApplicationContext, uid: str = None):
         return
 
     characters = showcase.get_character_showcase(uid)
+    current_showcase = pd.DataFrame.from_dict(characters, orient="index")
+    current_showcase["character_name"] = current_showcase.index
+
+    saved_showcase = db.get_saved_builds(db.showcase_db_connection(), uid)
+
+    curr = set(current_showcase["id"].to_list())
+    saved_showcase = saved_showcase[~saved_showcase["id"].isin(curr)]
+    showcase_total = pd.concat([current_showcase, saved_showcase])
+    builds.save(db.showcase_db_connection(), showcase_total, uid)
+
+    characters = json.loads(showcase_total.to_json(orient="index"))
     embed_field = "```\n"
     for character in characters:
         embed_field += "%-15s \n" % (
